76e2e63f213163700f41b705ee88bf7811c391f1
Tomasz Nowak <tnowak@triger.com.pl>
2020-04-11 22:34:00 +0200

    php7

--- src/Handlebars/Tokenizer.php
+++ src/Handlebars/Tokenizer.php
@@ -164,7 +164,7 @@ class Tokenizer
                 } elseif ($this->tagChange(self::T_UNESCAPED.$this->otag, $text, $i) and $this->escaped) {
                     $this->buffer .= "{{{";
                     $i += 2;
-                    continue;
+                    continue 2;
                 } elseif ($this->tagChange($this->otag, $text, $i) and (!$this->escaped || $prev_slash)) {
                     $i--;
                     $this->flushBuffer();
